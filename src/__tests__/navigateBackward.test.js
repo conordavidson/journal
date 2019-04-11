40545104fc20cae8ef3d9e7903dd9aa589b974a7
Conor Davidson <conor@conordavidson.com>
2019-04-10 23:28:33 -0400

    Init project

--- src/__tests__/navigateBackward.test.js
+++ src/__tests__/navigateBackward.test.js
@@ -1,51 +1,51 @@
-import navigateBackward from "../utils/navigateBackward";
+import navigateBackward from 'utils/navigateBackward';
 
-it("works", () => {
+it('works', () => {
   expect(navigateBackward(0, -1, 0, -1, 0, 0)).toEqual({
     chapter: -1,
     page: -1,
-    media: 0
+    media: 0,
   });
 
   expect(navigateBackward(1, 0, 0, -1, 0, 0, 4)).toEqual({
     chapter: -1,
     page: -1,
-    media: 3
+    media: 3,
   });
 
   expect(navigateBackward(1, 0, 2, 1, 0, 0)).toEqual({
     chapter: 0,
     page: 0,
-    media: 0
+    media: 0,
   });
 
   expect(navigateBackward(1, 0, 2, 1, 5, 4)).toEqual({
     chapter: 0,
     page: 1,
-    media: 3
+    media: 3,
   });
 
   expect(navigateBackward(1, 0, 2, 0, 5, 0)).toEqual({
     chapter: 0,
     page: -1,
-    media: 4
+    media: 4,
   });
 
   expect(navigateBackward(1, 0, 2, -1, 5, 0, 3)).toEqual({
     chapter: -1,
     page: 1,
-    media: 2
+    media: 2,
   });
 
   expect(navigateBackward(1, 0, 2, -1, 5, 0, 0)).toEqual({
     chapter: -1,
     page: 1,
-    media: 0
+    media: 0,
   });
 
   expect(navigateBackward(1, 0, 0, -1, 5, 0, 0)).toEqual({
     chapter: -1,
     page: -1,
-    media: 0
+    media: 0,
   });
 });
--- src/__tests__/navigateForward.test.js
+++ src/__tests__/navigateForward.test.js
@@ -1,57 +1,57 @@
-import navigateForward from "../utils/navigateForward";
+import navigateForward from 'utils/navigateForward';
 
-it("works", () => {
+it('works', () => {
   expect(navigateForward(0, -1, 0, -1, 0, 0)).toEqual({
     chapter: -1,
     page: -1,
-    media: 0
+    media: 0,
   });
 
   expect(navigateForward(1, -1, 0, -1, 0, 0)).toEqual({
     chapter: 0,
     page: -1,
-    media: 0
+    media: 0,
   });
 
   expect(navigateForward(5, 4, 0, -1, 0, 0)).toEqual({
     chapter: 4,
     page: -1,
-    media: 0
+    media: 0,
   });
 
   expect(navigateForward(5, 4, 3, 1, 0, 0)).toEqual({
     chapter: 4,
     page: 2,
-    media: 0
+    media: 0,
   });
 
   expect(navigateForward(5, 4, 3, 1, 1, 0)).toEqual({
     chapter: 4,
     page: 2,
-    media: 0
+    media: 0,
   });
 
   expect(navigateForward(5, 4, 3, 1, 3, 2)).toEqual({
     chapter: 4,
     page: 2,
-    media: 0
+    media: 0,
   });
 
   expect(navigateForward(5, 4, 1, 0, 3, 2)).toEqual({
     chapter: 4,
     page: 0,
-    media: 2
+    media: 2,
   });
 
   expect(navigateForward(5, 4, 1, 0, 4, 2)).toEqual({
     chapter: 4,
     page: 0,
-    media: 3
+    media: 3,
   });
 
   expect(navigateForward(6, 4, 3, 2, 4, 3)).toEqual({
     chapter: 5,
     page: -1,
-    media: 0
+    media: 0,
   });
 });
